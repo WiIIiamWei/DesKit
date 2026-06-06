b569de4c143ebccbbfb81a30c5b148a71d2252ce
William Wei <1397784681@qq.com>
2026-06-06 20:14:54 +0800

    fix: use DesKit app icons

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./global.css"
+import type { Metadata } from "next"
 import type { ReactNode } from "react"
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.svg",
+  },
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
